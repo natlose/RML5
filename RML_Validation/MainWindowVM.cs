b4c3b459df02b84a611c64f9ae684d60d24d1647
natlose <natlose@gmail.com>
2019-05-20 15:38:00 +0200

    ObservableObjectBase

---
+++ RML_Validation/MainWindowVM.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RML_Validation
+{
+    class MainWindowVM : ObservableObjectBase
+    {
+        MyData mydata;
+
+        private string idText;
+        public string IdText
+        {
+            get { return idText; }
+            set
+            {
+                idText = value;
+            }
+        }
+
+        private Person person;
+        public Person Person
+        {
+            get { return person; }
+            set {
+                person = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public void LoadData()
+        {
+            if (Int32.TryParse(idText, out int id))
+            {
+                mydata = new MyData();
+                Person result = mydata.Persons.SingleOrDefault(p => p.Id == id);
+                ValidationErrorsBegin(nameof(IdText));
+                if (result == null) ValidationErrorsAdd("Nem létezik.", nameof(IdText));
+                else Person = result;
+                ValidationErrorsEnd(nameof(IdText));
+            }
+        }
+
+        public void SaveData()
+        {
+            mydata.SaveChanges();
+        }
+
+    }
+}
